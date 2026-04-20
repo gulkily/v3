c4c4c6b87b0804bda034e5923a8dd64074aca9d1
Ilya Gulko <ilya@ilyagulko.com>
2026-04-20 19:55:56 -0400

    Simplify tools page cards

--- templates/pages/tools.php
+++ templates/pages/tools.php
@@ -7,7 +7,6 @@
   <article class="card">
     <h2><a href="<?= $e($toolPage['href']) ?>"><?= $e($toolPage['label']) ?></a></h2>
     <p><?= $e($toolPage['description']) ?></p>
-    <p><a class="nav-link" href="<?= $e($toolPage['href']) ?>"><?= $e($toolPage['label']) ?></a></p>
   </article>
 <?php endforeach; ?>
 </section>
